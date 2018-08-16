de8b5db2c2c6e67e8e4ac6dd26a6c40fc1302208
Valentin Dornauer <valentin@unimplemented.org>
2018-08-16 19:51:37 +0200

    restructure backend for jones

--- dev.sh
+++ dev.sh
@@ -4,6 +4,6 @@ docker volume create dwd || true
 
 tmux \
   new-session  "cd frontend/ && npm start; $SHELL" \; \
-  split-window "cd backend/ && docker build -t meteocool . && docker run -it --rm -v dwd:/usr/src/app/tmp meteocool && docker run --rm --name meteocool-tile -v dwd:/data -p 8070:80 klokantech/tileserver-gl ; $SHELL" \; \
-  split-window "echo hier muss der docker mit der backend ws app rein... ; read" \; \
+  split-window "cd backend/ && docker build -t meteocool . && docker run -it -v dwd:/usr/src/app/tmp -p 8071:8069 meteocool; $SHELL" \; \
+  split-window "docker run --rm --name meteocool-tile -v dwd:/data -p 8070:80 klokantech/tileserver-gl ; $SHELL" \; \
   select-layout even-vertical
