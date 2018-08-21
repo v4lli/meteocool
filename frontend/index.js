5ee31e50e45701d95683203da8d90295c59eade3
Jonas Wagner <boom@confuflux.com>
2018-08-21 18:16:23 +0200

    attempt on location #19

--- frontend/index.js
+++ frontend/index.js
@@ -4,8 +4,19 @@ import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj.js';
 import TileJSON from 'ol/source/TileJSON.js';
 import TileLayer from 'ol/layer/Tile.js';
-import {Map, View} from 'ol';
+import {Style, Fill, Stroke} from 'ol/style';
+import CircleStyle from 'ol/style/Circle';
+import {Map, View, Geolocation, Feature} from 'ol';
+import Point from 'ol/geom/Point';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 import io from 'socket.io-client';
+
+var view = new View({
+  center: fromLonLat([10.447683, 51.163375]),
+  zoom: 6,
+  minzoom: 5
+});
 
 const map = new Map({
 	target: 'map',
@@ -15,12 +26,61 @@ const map = new Map({
 		}),
 
 	],
-	view: new View({
-		center: fromLonLat([10.447683, 51.163375]),
-		zoom: 6,
-		minzoom: 5
-	})
+	view: view
 });
+
+var geolocation = new Geolocation({
+  // enableHighAccuracy must be set to true to have the heading value.
+  trackingOptions: {
+    enableHighAccuracy: true
+  },
+  projection: view.getProjection()
+});
+
+geolocation.setTracking(true);
+
+// update the HTML page when the position changes.
+geolocation.on('change', function() {
+  console.log('/dev/null');
+});
+
+// handle geolocation error.
+geolocation.on('error', function(error) {
+  console.log('could not get location data, doing nothing like it\'s 1990');
+});
+
+var accuracyFeature = new Feature();
+geolocation.on('change:accuracyGeometry', function() {
+  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+var positionFeature = new Feature();
+positionFeature.setStyle(new Style({
+  image: new CircleStyle({
+    radius: 6,
+    fill: new Fill({
+      color: '#3399CC'
+    }),
+    stroke: new Stroke({
+      color: '#fff',
+      width: 2
+    })
+  })
+}));
+
+geolocation.on('change:position', function() {
+  var coordinates = geolocation.getPosition();
+  positionFeature.setGeometry(coordinates ?
+    new Point(coordinates) : null);
+});
+
+new VectorLayer({
+  map: map,
+  source: new VectorSource({
+    features: [accuracyFeature, positionFeature]
+  })
+});
+
 
 if (process.env.NODE_ENV === 'production') {
 	var tileUrl = 'https://a.tileserver.unimplemented.org/data/raa01-wx_10000-latest-dwd-wgs84_transformed.json';
