361194a22902f3c6f300d1e3f496a3cec72fc72b
Florian Mauracher <flo@mauracher.eu>
2018-08-23 06:33:33 +0200

    Add webpack config

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+      use: ['url-loader']
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'assets/favicon.ico',
+      inject: true
+    }),
+    new CopyWebpackPlugin([
+      { from: 'assets' }
+    ]),
+  ]
+};
