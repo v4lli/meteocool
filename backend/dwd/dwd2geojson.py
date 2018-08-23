6c1edfe1e23c45214e356d8dc25cf6b13d25edef
Valentin Dornauer <valentin@unimplemented.org>
2018-08-23 03:20:37 +0200

    dwd: NOW WITH 8 BIT COLORS!

--- backend/dwd/dwd2geojson.py
+++ backend/dwd/dwd2geojson.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# this thing is a huge mess XXX
+
 import wradlib
 import geojson_utils
 from geojson_utils import number2radius, number2degree
@@ -7,30 +9,7 @@ import sys
 import math
 from geojson import Point, Polygon, FeatureCollection, Feature
 from PIL import Image
-
-
-def destination_point_coords(point, brng, dist):
-    """
-    Calculate a destination Point base on a base point and a distance
-    Keyword arguments:
-    pt   -- polygon geojson object
-    brng -- an angle in degrees
-    dist -- distance in Kilometer between destination and base point
-    return destination point object
-    """
-    dist = float(dist) / 6371  # convert dist to angular distance in radians
-    brng = number2radius(brng)
-
-    lon1 = number2radius(point[0])
-    lat1 = number2radius(point[1])
-
-    lat2 = math.asin(math.sin(lat1) * math.cos(dist) +
-                     math.cos(lat1) * math.sin(dist) * math.cos(brng))
-    lon2 = lon1 + math.atan2(math.sin(brng) * math.sin(dist) *
-                             math.cos(lat1), math.cos(dist) - math.sin(lat1) * math.sin(lat2))
-    lon2 = (lon2 + 3 * math.pi) % (2 * math.pi) - math.pi  # normalise to -180 degree +180 degree
-
-    return (number2degree(lon2), number2degree(lat2))
+import colorsys
 
 start = (46.9526, 3.5889)
 
@@ -41,49 +20,49 @@ data = wradlib.io.radolan.read_radolan_composite(sys.argv[1])
 # data is mm/5m, we convert it to dbz here: https://plot.ly/~ToniBois/1783.embed
 # https://www.dwd.de/DE/leistungen/radarniederschlag/rn_info/download_niederschlagsbestimmung.pdf?__blob=publicationFile&v=4
 
-def dbz2color(dbz):
-    return rel(dbz)
+def scale(r, g, b, num, idx):
+    threshold = 0.2
+    hls = colorsys.rgb_to_hls(r/255, g/255, b/255)
+    result = colorsys.hls_to_rgb(hls[0], hls[1]+(1-hls[1]-threshold)*(1/num)*idx, hls[2])
+    return (int(result[0]*255), int(result[1]*255), int(result[2]*255))
 
-def rel(dbz):
+def dbz2color(dbz):
     factor = (dbz % 5) + 1
     if dbz >= 170:
         return (0, 0, 0, 0)
     if dbz >= 75:
-        return (0xFE, 0xFC, 0xFD, int(255/5*factor))
+        return scale(0xFE, 0xFC, 0xFD, 95, dbz % 75)
     if dbz >= 70:
-        return (0x98, 0x58, 0xC4, int(255/5*factor))
+        return scale(0x98, 0x58, 0xC4, 5, dbz % 70)
     if dbz >= 65:
-        return (0xF6, 0x28, 0xF6, int(255/5*factor))
+        return scale(0xF6, 0x28, 0xF6, 5, dbz % 65)
     if dbz >= 60:
-        return (0xB8, 0x07, 0x11, int(255/5*factor))
+        return scale(0xB8, 0x07, 0x11, 5, dbz % 60)
     if dbz >= 55:
-        return (0xC9, 0x0B, 0x13, int(255/5*factor))
+        return scale(0xC9, 0x0B, 0x13, 5, dbz % 55)
     if dbz >= 50:
-        return (0xFA, 0x0D, 0x1C, int(255/5*factor))
+        return scale(0xFA, 0x0D, 0x1C, 5, dbz % 50)
     if dbz >= 45:
-        return (0xFA, 0x93, 0x26, int(255/5*factor))
+        return scale(0xFA, 0x93, 0x26, 5, dbz % 45)
     if dbz >= 40:
-        return (0xE3, 0xBB, 0x2A, int(255/5*factor))
+        return scale(0xE3, 0xBB, 0x2A, 5, dbz % 40)
     if dbz >= 35:
-        return (0xFC, 0xF3, 0x36, int(255/5*factor))
+        return scale(0xFC, 0xF3, 0x36, 5, dbz % 35)
     if dbz >= 30:
-        return (0x12, 0x8C, 0x15, int(255/5*factor))
+        return scale(0x12, 0x8C, 0x15, 5, dbz % 30)
     if dbz >= 25:
-        return (0x1E, 0xC4, 0x22, int(255/5*factor))
+        return scale(0x1E, 0xC4, 0x22, 5, dbz % 25)
     if dbz >= 20:
-        return (0x2A, 0xFC, 0x30, int(255/5*factor))
+        return scale(0x2A, 0xFC, 0x30, 5, dbz % 20)
     if dbz >= 15:
-        return (0x0E, 0x22, 0xEE, int(255/5*factor))
-    if dbz >= 10:
-        return (0x1B, 0xA0, 0xF0, int(129/5*factor))
+        return scale(0x0E, 0x22, 0xEE, 5, dbz % 15)
     if dbz >= 5:
-        return (0x1B, 0xA0, 0xF2, int(129/5*factor))
+        return scale(0x1B, 0xA0, 0xF0, 10, dbz % 5)
     if dbz >= 0:
-        return (0x00, 0xE7, 0xE7, int(129/5*factor))
+        return (64, 64, 64, int(50/5*factor))
     if dbz < 0:
         return (0x00, 0x00, 0x00, 0)
 
-cursor = tuple(start)
 feature_list = []
 rows = 0
 for row in reversed(data[0]):
@@ -91,33 +70,18 @@ for row in reversed(data[0]):
     for pixel in row:
         cols = cols + 1
         if pixel == -9999:
-            continue
-        if pixel < -31.9:
-            continue
-        if pixel > 172.6:
+            # this means no data
             continue
 
         try:
-            pixels[cols-1,rows] = dbz2color(pixel/2.0-32.5)
+            pixels[cols-1,rows] = dbz2color(wradlib.trafo.rvp_to_dbz(pixel))
         except IndexError:
             print("okay but why wtf IndexError cols=%d rows=%d" % (cols, rows))
             continue
-        # create a 1x1 square at cursor
-        #feature_list.append(
-        #        Feature(
-        #            geometry=Polygon([[cursor] +
-        #                [destination_point_coords(cursor, 45*x, round(math.sqrt(2),2) if x == 1 else 1) for x in range(0,3)] + [cursor]])))
-        cursor = destination_point_coords(cursor, 90, 1)
 
     rows = rows + 1
-    #print("Done with row=%d"%rows)
-    # "carriage return"
-    #cursor = destination_point_coords(start, 270, 1*cols)
-    # "newline"
-    #cursor = destination_point_coords(start, 0, 1*rows)
 
 print("biggest index row=%d col=%d" % (rows, cols))
 
-#boo = FeatureCollection(feature_list)
 img.show()
 img.save(sys.argv[2])
