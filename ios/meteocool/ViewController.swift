300eb6e1a01f879f7a665d9e878d499b028fdde0
Florian Mauracher <flo@mauracher.eu>
2018-08-25 01:07:14 +0200

    Create ios project

---
+++ ios/meteocool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meteocool
+//
+//  Created by Florian Mauracher on 25.08.18.
+//  Copyright © 2018 Florian Mauracher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
