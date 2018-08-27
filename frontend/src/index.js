9d96fb9267ff936ec69c34bfab5eb94b0dff736d
Valentin Dornauer <valentin@unimplemented.org>
2018-08-28 00:00:55 +0200

    frontend: optimize for iphone X

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -281,3 +281,16 @@ var locateControl = new Control({
 map.addControl(locateControl);
 
 console.log(map.getLayers());
+
+// Detects if device is on iOS
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  document.getElementById("clockbg").style.display = "block";
+}
