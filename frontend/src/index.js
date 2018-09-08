6e3e6144e0fecc960deaab6a978e8c0348dbf439
Valentin Dornauer <valentin@unimplemented.org>
2018-09-08 02:37:46 +0200

    frontend: fix stylechecker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -99,7 +99,7 @@ if (window.location.hash !== "") {
 var toggleButton = document.getElementById("toggleMode");
 var navbar = document.getElementById("navbar");
 
-var lightTiles = undefined; // this will use the OSM class' default - OSM doesn't offer pbf (vector) tiles (?)
+var lightTiles; // 'undefined' will use the OSM class' default - OSM doesn't offer pbf (vector) tiles (?)
 var darkTiles = "https://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.pbf";
 
 // light view is default
@@ -249,12 +249,18 @@ positionFeature.setStyle(new Style({
   })
 }));
 
+/* eslint-disable */
+/*
+ * Need to disable this stylechecker warning because the VectorLayer constructor
+ * us only used for its sideeffects, which isn't nice.
+ */
 new VectorLayer({
   map: map,
   source: new VectorSource({
     features: [accuracyFeature, positionFeature]
   })
 });
+/* eslint-enable */
 
 var haveZoomed = false;
 geolocation.on("change:position", function () {
