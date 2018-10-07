828f5a42473b40655e0d81261e56713a5328cfb5
Florian Mauracher <flo@mauracher.eu>
2018-10-07 20:37:37 +0200

    Migrate to safe area layout guides to fix fullscreen webview

--- ios/meteocool/ViewController.swift
+++ ios/meteocool/ViewController.swift
@@ -10,23 +10,20 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKUIDelegate {
-    let webView = WKWebView()
+    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        UIApplication.shared.statusBarView?.backgroundColor = UIColor.black
 
         if let url = URL(string: "https://unimplemented.org/meteocool/") {
             let request = URLRequest(url: url)
             webView.load(request)
         }
     }
+}
 
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-
-    override func loadView() {
-        self.view = webView
+extension WKWebView {
+    override open var safeAreaInsets: UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
