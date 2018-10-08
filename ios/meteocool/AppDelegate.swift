c133aeda44682285801cb72c621a16f6ac54da10
Florian Mauracher <flo@mauracher.eu>
2018-10-08 05:13:48 +0200

    Add background location monitoring in preparation for weather alerts

--- ios/meteocool/AppDelegate.swift
+++ ios/meteocool/AppDelegate.swift
@@ -1,21 +1,15 @@
-//
-//  AppDelegate.swift
-//  meteocool
-//
-//  Created by Florian Mauracher on 25.08.18.
-//  Copyright © 2018 Florian Mauracher. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var locationUpdater:LocationUpdater!
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.locationUpdater = LocationUpdater.init()
         return true
     }
 
@@ -40,8 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
 extension UIApplication {
