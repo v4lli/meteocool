c21077548ca0d3520d5511f2119f3b56be97bb8c
Valentin Dornauer <valentin@unimplemented.org>
2018-10-15 22:55:25 +0200

    frontend: integrate legend

--- backend/dwd/color_legend.py
+++ backend/dwd/color_legend.py
@@ -3,12 +3,21 @@ import colorsys
 import sys
 from dwd2geojson import scale, dbz2color
 
-size = (2048, 200)
-image = Image.new("RGBA", size)
+#background = Image.open("osm_munich.png")
+
+size = (1536, 300)
+image = Image.new("RGBA", size, "black")
 draw = ImageDraw.Draw(image)
 
 count = 0
-for color in range(-32, 95):
-    draw.line((count, 0, count, image.size[0]), fill=dbz2color(color), width=8)
+for color in range(0, 96*2):
+    draw.line((count, 0, count, image.size[0]), fill=dbz2color(color/2.0), width=8)
     count = count + 8
+draw.line((count+1, 0, count+1, image.size[0]), fill=(0xff, 0xff, 0xff, 0), width=8)
 image.save(sys.argv[1])
+
+#background.paste(image, (0, 0), image)
+#cropped = background.crop((0, 0, size[0], size[1]))
+#
+#cropped.save(sys.argv[1])
+#cropped.show()
