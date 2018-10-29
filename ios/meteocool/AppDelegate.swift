313c6ca000c9d3d7e0cc18c901085c0ad145b48f
Valentin Dornauer <valentin@unimplemented.org>
2018-10-29 22:45:31 +0100

    ios: process remote notifications
    This allows us to receive so called "silent push" notifications. We
    use those to clear any pending notification if it was not acknowledged
    before the rain stopped. No use in displaying and accumulating
    notifications of past rain.

--- ios/meteocool/AppDelegate.swift
+++ ios/meteocool/AppDelegate.swift
@@ -36,6 +36,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print(userInfo)
+        if  let clear_all = userInfo["clear_all"] as? Bool {
+            if (clear_all) {
+                self.notificationManager.clearNotifications()
+            }
+        }
+    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
