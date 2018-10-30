1e7ace5a8f81f99ec55c7dcb5f757fbe407424dc
Valentin Dornauer <valentin@unimplemented.org>
2018-10-30 04:07:06 +0100

    backend: push backend
    Implements the push backend used by both the iOS and the webbrowser
    socket.
    
    While there, clean up the DWD makefile.

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,6 +5,7 @@ eventlet.monkey_patch()
 
 import logging
 import json
+import uuid
 import websocket
 import threading
 from pyproj import Proj, transform
@@ -35,16 +36,20 @@ def update_all_clients(newTileJson):
 def publish_tileset():
     data = request.get_json()
     if data:
+        logging.warn("starting background task")
         socketio.start_background_task(update_all_clients, data)
+        logging.warn("finished starting background task")
         return "OK"
     else:
         return "ERROR"
+
 
 # Public API endpoint, used by mobile devies and browsers to
 # register notification requests for incoming rain. Expects a
 # JSON dict containing the following keys:
 #  - lat: float
 #  - lon: float
+#  - ahead: int - look n minutes into the future (5 minute steps, max=60)
 #  - source: string - "ios" or "browser"
 #  - token: string - for source=browser, a random identifier
 #    used by the push-handling backend to associate a websocket
@@ -52,8 +57,10 @@ def publish_tileset():
 #    APNS push token.
 @app.route("/post_location", methods=["POST"])
 def post_location():
-    data = request.get_json()
+    logging.warn(request.get_json())
+    return save_location_to_backend(request.get_json())
 
+def save_location_to_backend(data):
     if not data:
         return jsonify(success=False, message="bad request")
 
@@ -61,6 +68,8 @@ def post_location():
         lat = data["lat"]
         lon = data["lon"]
         source = data["source"]
+        ahead = data["ahead"]
+        intensity = data["intensity"]
         token = data["token"]
         accuracy = data["accuracy"]
     except KeyError:
@@ -68,25 +77,31 @@ def post_location():
     else:
         if not isinstance(lat, float) or not isinstance(lon, float):
             return jsonify(success=False, message="bad lat/lon")
-        if not isinstance(accuracy, float):
+        if not isinstance(accuracy, float) and not isinstance(accuracy, int):
             return jsonify(success=False, message="invalid accuracy")
         if source != "browser" and source != "ios":
             return jsonify(success=False, message="bad source")
         if not isinstance(token, str) or len(token) > 128 or len(token) < 32:
             return jsonify(success=False, message="bad token")
+        if not isinstance(ahead, int) or ahead < 0 or ahead > 60:
+            return jsonify(success=False, message="invalid ahead value")
+        if not isinstance(intensity, int) or intensity < 0 or intensity > 130:
+            return jsonify(success=False, message="invalid intensity value")
 
+        # XXX this might override ios_onscreen! FIXME
         data = {
             "lat": lat,
             "lon": lon,
             "accuracy": accuracy,
             "last_updated": datetime.datetime.utcnow(),
-            "dbz": 42,
+            "intensity": intensity,
+            "ahead": ahead,
             "source": source,
+            "ios_onscreen": False,
             "token": token
         }
-
-        geolocation = db.collection
-        post_location = geolocation.insert_one(data).inserted_id
+        key = {"token": token}
+        db.collection.update(key, data, upsert=True)
 
     return jsonify(success=True)
 
@@ -94,12 +109,54 @@ def post_location():
 # Executed when a new websocket client connects. Currently no-op.
 @socketio.on("connect", namespace="/tile")
 def log_connection():
-    logging.info("client connected")
+    logging.warn("client connected")
 
+# Called by the browser to register push notifications
+pushable_browsers = []
+
+@socketio.on("register", namespace="/rain_notify_browser")
+def register_browser_push(json):
+    global pushable_browsers
+    rand_uuid = str(uuid.uuid4())
+
+    pushable_browsers.append({"sid": request.sid, "uuid": rand_uuid})
+
+    logging.warn("registering browser push: %s" % str(json))
+
+    ret = save_location_to_backend({
+        "lat": json["lat"],
+        "lon": json["lon"],
+        "ahead": json["ahead"],
+        "accuracy": 1.0,
+        "intensity": json["intensity"],
+        "source": "browser",
+        "token": rand_uuid})
+    logging.warn(ret.data)
+    return ret
+
+# Called by the browser to register push notifications
+@socketio.on("unregister", namespace="/rain_notify_browser")
+def register_browser_push(json):
+    # XXX implement me
+    logging.warn("UNregistering browser push: %s" % str(json))
+
+@app.route("/internal/trigger_browser_notification", methods=["POST"])
+def trigger_browser_notification():
+    data = request.get_json()
+
+    for browser in pushable_browsers:
+        if data["token"] == browser["uuid"]:
+            # XXX we need some kind of feedback from socketio here. if the notification can't be delivered, it needs to be removed from the database
+            # (like in push.py)
+            socketio.emit("notify", {
+                "title": ("Rain expected in %d minutes!" % data["ahead"]),
+                "body": ("DWD reports that it might rain at your current location in about %d minutes." % data["ahead"])
+            }, room=browser["sid"])
+
+    return jsonify(success=True)
 
 numStrikes = 0
 failStrikes = 0
-
 
 def blitzortung_thread():
     """i connect to blitzortung.org and forward ligtnings to clients in my namespace"""
@@ -151,18 +208,20 @@ def blitzortung_thread():
         print("### closed ###")
 
     def on_open(ws):
-        ws.send(json.dumps({"west": -20.0, "east": 44.0, "north": 71.5, "south": 23.1}))
+        ws.send(json.dumps({
+            "west": -20.0,
+            "east": 44.0,
+            "north": 71.5,
+            "south": 23.1}))
 
     def stats_logging_cb():
-        logging.warn(
-            "Processed %d strikes since last report (%d failed)"
-            % (getAndResetStrikes(), getAndResetFailStrikes())
-        )
-        threading.Timer(10, stats_logging_cb).start()
+        logging.warn("Processed %d strikes since last report (%d failed)"
+            % (getAndResetStrikes(), getAndResetFailStrikes()))
+        threading.Timer(5*60, stats_logging_cb).start()
 
     logging.warn("blitzortung thread init")
     websocket.enableTrace(True)
-    threading.Timer(5 * 60, stats_logging_cb).start()
+    stats_logging_cb()
 
     while True:
         # XXX error handling
