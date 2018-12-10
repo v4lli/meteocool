80d731d5071336b9f55845a6f62d807d6f339ea5
Valentin Dornauer <valentin@unimplemented.org>
2018-12-10 17:24:01 +0100

    refine colors

--- backend/dwd/dbz2color.py
+++ backend/dwd/dbz2color.py
@@ -25,43 +25,43 @@ def dbz2color(dbz):
     factor = (dbz % 5) + 1
     if dbz >= 170:
         return (0, 0, 0, 0)
-    if dbz >= 74:
-        return scale(0xFE, 0xFC, 0xFD, 93, dbz % (74))
-    if dbz >= 71:
-        return scale(0xFE, 0xFC, 0xFD, 3, dbz % (71))
-    if dbz >= 65:
-        return scale(0x98, 0x58, 0xC4, 6, dbz % (65))
-    if dbz >= 50:
-        return scale(0xF6, 0x28, 0xF6, 15, dbz % (50))
+    if dbz >= 70:
+        return scale(0xFE, 0xFC, 0xFD, 90, dbz % (70))
+    if dbz >= 55:
+        return scale(0xFE, 0xFC, 0xFD, 15, dbz % (55))
+    if dbz >= 45:
+        return scale(0x98, 0x58, 0xC4, 10, dbz % (45))
+    if dbz >= 40:
+        return scale(0xF6, 0x28, 0xF6, 5, dbz % (40))
     # if dbz >= 60:
     #    return scale(0xB8, 0x07, 0x11, 5, dbz % (60))
     # redundant color
     # if dbz >= 55:
     #    return scale(0xC9, 0x0B, 0x13, 5, dbz % (55))
     if dbz >= 35:
-        return scale(117, 0, 0, 15, dbz % (35), False, True)
+        return scale(117, 0, 0, 5, dbz % (35), False, True)
     # if dbz >= 45:
     #    return scale(0xFA, 0x93, 0x26, 5, dbz % (45))
     # ugly color
     # if dbz >= 40:
     #    return scale(0xE3, 0xBB, 0x2A, 5, dbz % (40))
-    if dbz >= 32:
-        # gelb
-        return scale(87, 74, 0, 3, dbz % (32))
     if dbz >= 30:
+        # gelb
+        return scale(87, 74, 0, 5, dbz % (30))
+    if dbz >= 25:
         # gruen
-        return scale(0x12, 0x8C, 0x15, 2, dbz % (30), True)
+        return scale(0x12, 0x8C, 0x15, 5, dbz % (25), True)
     # also redundant
     # if dbz >= 25:
     #    return scale(0x1E, 0xC4, 0x22, 5, dbz % (25))
-    if dbz >= 25:
+    if dbz >= 20:
         # return scale(0x2A, 0xFC, 0x30, 5, dbz % (20))
         # the redundant color looks nicer though...
-        return scale(0x1E, 0xC4, 0x22, 5, dbz % (25))
-    if dbz >= 20:
-        return scale(0x0E, 0x22, 0xEE, 10, dbz % 20)
+        return scale(0x1E, 0xC4, 0x22, 5, dbz % (20))
+    if dbz >= 15:
+        return scale(0x0E, 0x22, 0xEE, 5, dbz % 15)
     if dbz >= 10:
-        return scale(0x0E, 0x22, 0xEE, 10, dbz % 10, True)
+        return scale(0x0E, 0x22, 0xEE, 5, dbz % 10, True)
     if dbz >= 0:
         return (64, 64, 64, int(115 / 5 * factor))
     if dbz < 0:
