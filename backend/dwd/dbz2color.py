1c9e4db0bde6c85605859625424f45570b234756
Valentin Dornauer <valentin@unimplemented.org>
2019-01-07 00:39:30 +0100

    brush up palette

--- backend/dwd/dbz2color.py
+++ backend/dwd/dbz2color.py
@@ -22,47 +22,45 @@ def scale(r, g, b, num, idx, alpha=False, lightToDark=True):
 
 
 def dbz2color(dbz):
-    factor = (dbz % 5) + 1
     if dbz >= 170:
         return (0, 0, 0, 0)
-    if dbz >= 70:
-        return scale(0xFE, 0xFC, 0xFD, 10, dbz % (70), False, True)
-    if dbz >= 55:
-        return scale(0xFE, 0xFC, 0xFD, 15, dbz % (55))
-    if dbz >= 45:
-        return scale(0x98, 0x58, 0xC4, 10, dbz % (45))
-    if dbz >= 40:
-        return scale(0xF6, 0x28, 0xF6, 5, dbz % (40))
+    if dbz >= 61:
+        return scale(0xFE, 0xFC, 0xFD, 10, dbz % (61), False, True)
+    if dbz >= 46:
+        return scale(0xFE, 0xFC, 0xFD, 15, dbz % (51))
+    if dbz >= 36:
+        return scale(0x98, 0x58, 0xC4, 10, dbz % (36))
+    if dbz >= 31:
+        return scale(0xF6, 0x28, 0xF6, 5, dbz % (31))
     # if dbz >= 60:
     #    return scale(0xB8, 0x07, 0x11, 5, dbz % (60))
     # redundant color
     # if dbz >= 55:
     #    return scale(0xC9, 0x0B, 0x13, 5, dbz % (55))
-    if dbz >= 35:
-        return scale(117, 0, 0, 5, dbz % (35), False, True)
+    if dbz >= 28:
+        # red
+        return scale(117, 0, 0, 5, dbz % (28), False, True)
     # if dbz >= 45:
     #    return scale(0xFA, 0x93, 0x26, 5, dbz % (45))
     # ugly color
     # if dbz >= 40:
     #    return scale(0xE3, 0xBB, 0x2A, 5, dbz % (40))
-    if dbz >= 30:
-        # gelb
-        return scale(87, 74, 0, 5, dbz % (30))
-    if dbz >= 25:
-        # gruen
-        return scale(0x12, 0x8C, 0x15, 5, dbz % (25), True)
+    if dbz >= 23:
+        # yellow
+        return scale(87, 74, 0, 4, dbz % (23))
     # also redundant
     # if dbz >= 25:
     #    return scale(0x1E, 0xC4, 0x22, 5, dbz % (25))
-    if dbz >= 20:
+    if dbz >= 18:
         # return scale(0x2A, 0xFC, 0x30, 5, dbz % (20))
         # the redundant color looks nicer though...
-        return scale(0x1E, 0xC4, 0x22, 5, dbz % (20))
-    if dbz >= 15:
-        return scale(0x0E, 0x22, 0xEE, 5, dbz % 15)
-    if dbz >= 10:
-        return scale(0x0E, 0x22, 0xEE, 5, dbz % 10, True)
+        # green
+        return scale(0x1E, 0xC4, 0x22, 5, dbz % (17))
+    if dbz >= 12:
+        # blau
+        return scale(0x0E, 0x22, 0xEE, 6, dbz % 12)
     if dbz >= 0:
-        return (64, 64, 64, int(115 / 5 * factor))
+        # grey
+        return (70, 70, 70, int(105 / 12 * (dbz+1))%255)
     if dbz < 0:
         return (0x00, 0x00, 0x00, 0)
