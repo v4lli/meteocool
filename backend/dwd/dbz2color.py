a105dd8a463e8f39f45eec161f713010fe700450
Valentin Dornauer <valentin@unimplemented.org>
2019-01-07 01:09:19 +0100

    more palette refinement

--- backend/dwd/dbz2color.py
+++ backend/dwd/dbz2color.py
@@ -1,8 +1,7 @@
 import colorsys
 
 
-def scale(r, g, b, num, idx, alpha=False, lightToDark=True):
-    threshold = 0.2
+def scale(r, g, b, num, idx, alpha=False, lightToDark=True, threshold = 0.2):
     hls = colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
     if lightToDark:
         result = colorsys.hls_to_rgb(
@@ -14,7 +13,7 @@ def scale(r, g, b, num, idx, alpha=False, lightToDark=True):
         )
 
     if alpha:
-        a = 150 + int(105 / (num + 2) * (idx + 1))
+        a = 200 + int(55 / (num + 2) * (idx + 1))
     else:
         a = 255
 
@@ -24,22 +23,22 @@ def scale(r, g, b, num, idx, alpha=False, lightToDark=True):
 def dbz2color(dbz):
     if dbz >= 170:
         return (0, 0, 0, 0)
-    if dbz >= 61:
-        return scale(0xFE, 0xFC, 0xFD, 10, dbz % (61), False, True)
-    if dbz >= 46:
-        return scale(0xFE, 0xFC, 0xFD, 15, dbz % (51))
-    if dbz >= 36:
-        return scale(0x98, 0x58, 0xC4, 10, dbz % (36))
-    if dbz >= 31:
-        return scale(0xF6, 0x28, 0xF6, 5, dbz % (31))
+    if dbz >= 62:
+        return scale(0xFE, 0xFC, 0xFD, 10, dbz % (62), False, True)
+    if dbz >= 47:
+        return scale(0xFE, 0xFC, 0xFD, 15, dbz % (47))
+    if dbz >= 37:
+        return scale(0x98, 0x58, 0xC4, 10, dbz % (37))
+    if dbz >= 32:
+        return scale(0xF6, 0x28, 0xF6, 5, dbz % (32))
     # if dbz >= 60:
     #    return scale(0xB8, 0x07, 0x11, 5, dbz % (60))
     # redundant color
     # if dbz >= 55:
     #    return scale(0xC9, 0x0B, 0x13, 5, dbz % (55))
-    if dbz >= 28:
+    if dbz >= 27:
         # red
-        return scale(117, 0, 0, 5, dbz % (28), False, True)
+        return scale(117, 0, 0, 5, dbz % (27), False, True)
     # if dbz >= 45:
     #    return scale(0xFA, 0x93, 0x26, 5, dbz % (45))
     # ugly color
@@ -47,7 +46,7 @@ def dbz2color(dbz):
     #    return scale(0xE3, 0xBB, 0x2A, 5, dbz % (40))
     if dbz >= 23:
         # yellow
-        return scale(87, 74, 0, 4, dbz % (23))
+        return scale(87, 74, 0, 4, dbz % (23), threshold=0.5)
     # also redundant
     # if dbz >= 25:
     #    return scale(0x1E, 0xC4, 0x22, 5, dbz % (25))
@@ -55,12 +54,12 @@ def dbz2color(dbz):
         # return scale(0x2A, 0xFC, 0x30, 5, dbz % (20))
         # the redundant color looks nicer though...
         # green
-        return scale(0x1E, 0xC4, 0x22, 5, dbz % (17))
+        return scale(0x1E, 0xC4, 0x22, 5, dbz % (18))
     if dbz >= 12:
         # blau
-        return scale(0x0E, 0x22, 0xEE, 6, dbz % 12)
+        return scale(0x0E, 0x22, 0xEE, 6, dbz % 12, True)
     if dbz >= 0:
         # grey
-        return (70, 70, 70, int(105 / 12 * (dbz+1))%255)
+        return (0, 0, 0, int(105 / 12 * (dbz+1))%255)
     if dbz < 0:
         return (0x00, 0x00, 0x00, 0)
