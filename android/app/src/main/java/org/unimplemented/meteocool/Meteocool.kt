69da243f4622555131d5d51e448e450e4f47f062
jerem <wiedman0@hm.edu>
2019-01-30 11:19:53 +0100

    Initial android commit

---
+++ android/app/src/main/java/org/unimplemented/meteocool/Meteocool.kt
@@ -0,0 +1,12 @@
+package org.unimplemented.meteocool
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Meteocool : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meteocool)
+    }
+}
