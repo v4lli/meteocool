f05208f666e67cfa2a43295d11dd3abbc2f129b1
jerem <wiedman0@hm.edu>
2019-01-31 11:12:59 +0100

    Implemented metecool as webview. Updated AS dependency versions

--- android/app/src/main/java/org/unimplemented/meteocool/Meteocool.kt
+++ android/app/src/main/java/org/unimplemented/meteocool/Meteocool.kt
@@ -2,11 +2,24 @@ package org.unimplemented.meteocool
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+
+
 
 class Meteocool : AppCompatActivity() {
 
+    companion object {
+       const val WEB_URL = "https://meteocool.unimplemented.org/?mobile=android"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_meteocool)
+        val myWebView = WebView(this)
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.databaseEnabled = true
+        setContentView(myWebView)
+        myWebView.loadUrl("https://meteocool.unimplemented.org/?mobile=android")
     }
 }
