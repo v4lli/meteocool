2f454a32378d0442c2b802d924cced4dd5b7dd9c
NessArt <nessie_smile@yahoo.de>
2019-02-01 13:16:13 +0100

    push notifications

--- android/app/src/main/java/org/unimplemented/meteocool/Meteocool.kt
+++ android/app/src/main/java/org/unimplemented/meteocool/Meteocool.kt
@@ -2,7 +2,13 @@ package org.unimplemented.meteocool
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
 import android.webkit.WebView
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 
 
 
@@ -21,5 +27,18 @@ class Meteocool : AppCompatActivity() {
         webSettings.databaseEnabled = true
         setContentView(myWebView)
         myWebView.loadUrl("https://meteocool.unimplemented.org/?mobile=android")
+
+        var preference = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        Log.d("Preferences", preference.getString("FIREBASE_TOKEN", "error"))
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.notifications_admin_channel_id)
+            val channelName = getString(R.string.notifications_admin_channel_name)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_LOW))
+        }
     }
 }
