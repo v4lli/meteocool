1ced7f4f3539d65ac70f4098ceeb33235377916a
jerem <wiedman0@hm.edu>
2019-02-04 10:11:00 +0100

    Started location listener

---
+++ android/app/src/main/java/org/unimplemented/meteocool/MyLocationListener.kt
@@ -0,0 +1,24 @@
+package org.unimplemented.meteocool
+
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+
+class MyLocationListener : LocationListener {
+    override fun onLocationChanged(location: Location?) {
+
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ android/app/src/main/java/org/unimplemented/meteocool/Utility.kt
@@ -0,0 +1,5 @@
+package org.unimplemented.meteocool
+
+class Utility{
+    
+}
