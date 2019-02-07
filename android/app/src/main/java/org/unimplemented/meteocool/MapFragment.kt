eb828f3ad7cd7b223ac2fcc72232bd933b8f5f77
jerem <wiedman0@hm.edu>
2019-02-07 15:40:56 +0100

    Removed unnecessary code

--- android/app/src/main/java/org/unimplemented/meteocool/MapFragment.kt
+++ android/app/src/main/java/org/unimplemented/meteocool/MapFragment.kt
@@ -1,7 +1,5 @@
 package org.unimplemented.meteocool
 
-import android.app.NotificationManager
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
--- android/app/src/main/java/org/unimplemented/meteocool/Meteocool.kt
+++ android/app/src/main/java/org/unimplemented/meteocool/Meteocool.kt
@@ -19,8 +19,6 @@ import org.unimplemented.meteocool.service.UploadLocationService
 
 class Meteocool : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meteocool)
@@ -34,40 +32,11 @@ class Meteocool : AppCompatActivity() {
         Log.d("Preferences", preference.getString("FIREBASE_TOKEN", "error"))
 
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            // Create channel to show notifications.
-//            val channelId = getString(R.string.notifications_admin_channel_id)
-//            val channelName = getString(R.string.notifications_admin_channel_name)
-//            val notificationManager = getSystemService(NotificationManager::class.java)
-//            notificationManager?.createNotificationChannel(NotificationChannel(channelId,
-//                channelName, NotificationManager.IMPORTANCE_LOW))
-//        }
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if(notificationManager.activeNotifications.isNotEmpty()) {
-            notificationManager.cancelAll()
-            var token = FirebaseInstanceId.getInstance().token
-            if (token == null) {
-                token = "no token"
-            }
-            doAsync {
-                NetworkUtility.sendClearPostRequest(
-                    JSONClearPost(
-                        token,
-                        "backend"
-                    )
-                )
-            }
-        }
         supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, MapFragment()).commit()
-
     }
-
-
 
     override fun onResume() {
         super.onResume()
-
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if(notificationManager.activeNotifications.isNotEmpty()) {
             notificationManager.cancelAll()
@@ -84,19 +53,6 @@ class Meteocool : AppCompatActivity() {
                 )
             }
         }
-        /*doAsync { NetworkUtility.sendPostRequest(
-            JSONPost(1.0,
-            1.0,
-            1.1,
-            1.1f,
-            1.0f,
-            123.0,
-            1234.0,
-            "anon",
-            "android",
-            15,
-            10)
-        )} // currently for testing*/
     }
 }
 
