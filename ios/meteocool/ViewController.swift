4b1efdb5bd94f84a8a811ec6424a1565b7fbfbf4
nina2244 <26925042+Nina2244@users.noreply.github.com>
2019-02-08 08:33:05 +0100

    forcast can be moved by slider

--- ios/meteocool/ViewController.swift
+++ ios/meteocool/ViewController.swift
@@ -1,9 +1,20 @@
 import UIKit
 import WebKit
+import UIKit.UIGestureRecognizer
 
 class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler {
+    var buttonsize = 19.0 as CGFloat
+    
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var slider_ring: UIImageView!
+    @IBOutlet weak var slider_button: UIImageView!
 
+    func move_slider_button(pointToMove: CGPoint){
+        let x_coordiante = (pointToMove.x)-(buttonsize/2)
+        let y_coordinate = (pointToMove.y)-(buttonsize/2)
+        slider_button.frame.origin = CGPoint(x:x_coordiante,y:y_coordinate)
+    }
+    
     func toggleDarkMode() {
         // #343a40 = darkmode titelbar color
         let darkmode = UIColor(red: 0x34/255.0, green: 0x3a/255.0, blue: 0x40/255.0, alpha: 1.0)
@@ -32,6 +43,12 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler {
         super.loadView()
         webView?.configuration.userContentController.add(self, name: "scriptHandler")
         self.view.addSubview(webView!)
+        self.view.addSubview(slider_ring!)
+        self.view.addSubview(slider_button!)
+        
+        let gesture = CustomGestureRecognizer(target: self, action: nil)
+        gesture.setView(viewing: self)
+        view.addGestureRecognizer(gesture) // #selector(UIViewController.viewDidLoad)
     }
 
     override func viewDidLoad() {
