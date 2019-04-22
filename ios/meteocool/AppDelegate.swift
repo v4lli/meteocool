f8632a3c8c8881bf0cf29f4e7ddda58ddd46a67d
Valentin Dornauer <valentin@unimplemented.org>
2019-04-22 22:08:05 +0200

    ios: save some requests when in task switcher

--- ios/meteocool/AppDelegate.swift
+++ ios/meteocool/AppDelegate.swift
@@ -27,13 +27,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         SharedNotificationManager.clearNotifications()
-
-        // XXX call this only when there are >0 notifications on launch! saves 1 useless request.
-        acknowledgeNotification(retry: true, from: "foreground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+        // XXX call this only when there are >0 notifications on launch! saves 1 useless request.
+        acknowledgeNotification(retry: true, from: "foreground")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
