7e852e1aaf9306f5fd618beee0aed05c528e670f
Jeremias Wiedmann <wiedman0@hm.edu>
2019-05-09 16:48:10 +0200

    Added simple error handling for cannot upload location data

--- android/app/src/main/java/org/unimplemented/meteocool/utility/NetworkUtility.kt
+++ android/app/src/main/java/org/unimplemented/meteocool/utility/NetworkUtility.kt
@@ -3,6 +3,7 @@ package org.unimplemented.meteocool.utility
 import android.util.Log
 import com.google.gson.Gson
 import java.io.*
+import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -23,34 +24,37 @@ companion object {
 
         val jsonAsString = buildJSONString(json)
 
+        try {
+            with(url.openConnection() as HttpURLConnection) {
+                // optional default is GET
+                requestMethod = "POST"
+                setRequestProperty("charset", "utf-8")
+                setRequestProperty("Content-lenght", jsonAsString.length.toString())
+                setRequestProperty("Content-Type", "application/json")
 
-        with(url.openConnection() as HttpURLConnection) {
-            // optional default is GET
-            requestMethod = "POST"
-            setRequestProperty("charset", "utf-8")
-            setRequestProperty("Content-lenght", jsonAsString.length.toString())
-            setRequestProperty("Content-Type", "application/json")
+                val wr = OutputStreamWriter(outputStream)
 
-            val wr = OutputStreamWriter(outputStream)
+                wr.write(jsonAsString)
+                wr.flush()
 
-            wr.write(jsonAsString)
-            wr.flush()
-
-            Log.d("NetworkUtility", "URL $url")
-            Log.d("NetworkUtility", "HTTP-Response $responseCode")
+                Log.d("NetworkUtility", "URL $url")
+                Log.d("NetworkUtility", "HTTP-Response $responseCode")
 
 
-            BufferedReader(InputStreamReader(inputStream)).use {
-                val response = StringBuffer()
+                BufferedReader(InputStreamReader(inputStream)).use {
+                    val response = StringBuffer()
 
-                var inputLine = it.readLine()
-                while (inputLine != null) {
-                    response.append(inputLine)
-                    inputLine = it.readLine()
+                    var inputLine = it.readLine()
+                    while (inputLine != null) {
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    it.close()
+                    Log.d("NetworkUtility", "$response")
                 }
-                it.close()
-                Log.d("NetworkUtility", "$response")
             }
+        }catch(e : Exception){
+            Log.d("NetworkUtility", "Upload Error")
         }
     }
 }
