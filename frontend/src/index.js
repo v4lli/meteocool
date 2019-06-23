302396bc545e19d7e9b2f900e6ae98b88e21360d
Valentin Dornauer <valentin@unimplemented.org>
2019-06-23 10:35:33 +0200

    fix build for dev environment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -382,11 +382,11 @@ window.geolocation.on("change:position", () => {
 //
 
 var tileUrl = "http://localhost:8041/data/raa01-wx_10000-latest-dwd-wgs84_transformed.json";
-//var websocketUrl = "https://meteocool.com/tile";
-var websocketUrl = "http://localhost:8040/tile";
-// if (process.env.NODE_ENV === "production") {
-//tileUrl = "https://a.tileserver.unimplemented.org/data/raa01-wx_10000-latest-dwd-wgs84_transformed.json";
-// }
+var websocketUrl = "https://meteocool.com/tile";
+if (process.env.NODE_ENV === "production") {
+  tileUrl = "https://a.tileserver.unimplemented.org/data/raa01-wx_10000-latest-dwd-wgs84_transformed.json";
+  websocketUrl = "http://localhost:8040/tile";
+}
 
 var reflectivityOpacity = 0.5;
 
